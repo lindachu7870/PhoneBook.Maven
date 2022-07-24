1ced1e5466efce10117df32330532a12e59c7355
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-23 21:37:59 -0400

    almost done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -16,28 +13,38 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this.phonebook = new LinkedHashMap<>();
     }
 
-    public void add(String name, String phoneNumber) {
+    public void add(String name, String phoneNum) {
+        List<String> phoneNumList = new ArrayList<>();
+        phoneNumList.add(phoneNum);
+        this.phonebook.put(name, phoneNumList);
     }
 
-    public void addAll(String name, String... phoneNumbers) {
+    public void addAll(String name, String... phoneNum) {
+        List<String> list = Arrays.asList(phoneNum);
+        List<String> phoneNumList = new ArrayList<>(list);
+        this.phonebook.put(name, phoneNumList);
     }
 
     public void remove(String name) {
+        this.phonebook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        if (phonebook.containsKey(name)) {
+            return true;
+        }
+        return false;
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return phonebook.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
